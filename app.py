a31cf81502e44602ea568de7034b6d0740853db8
Erick Sanchez <erickfernandosanchezbarraza@gmail.com>
2025-05-20 23:35:49 -0700

    Corrijo app.py para puerto y host, actualizo requirements.txt

--- app.py
+++ app.py
@@ -126,4 +126,7 @@ def consulta():
     return render_template("consulta.html", medicamentos=medicamentos)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Railway asigna un puerto dinámico
+    app.run(host="0.0.0.0", port=port)
+
